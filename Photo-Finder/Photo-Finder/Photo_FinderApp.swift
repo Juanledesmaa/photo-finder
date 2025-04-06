d5b1e7af839ca952d5985f60dfe8025a0f436fd1
juanledesmaa <umbranito@gmail.com>
2025-04-05 21:46:43 -0400

    Add all baseline project files with SwiftUI and Swift 6

---
+++ Photo-Finder/Photo-Finder/Photo_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_FinderApp.swift
+//  Photo-Finder
+//
+//  Created by Juanito on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Photo_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
